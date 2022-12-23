88c31430dd48bbd7255e38adb664e81f21358ba1
Heis97 <46742591+Heis97@users.noreply.github.com>
2022-12-23 10:05:25 +0300

    2312

--- TensomUI/Properties/Resources.Designer.cs
+++ TensomUI/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TensomUI.Properties {
+namespace Spher_scara.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace TensomUI.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TensomUI.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Spher_scara.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- TensomUI/Properties/Settings.Designer.cs
+++ TensomUI/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TensomUI.Properties {
+namespace Spher_scara.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
